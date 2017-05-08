282200c9011cf1900b0597e9b46336f5197718ae
jay <jayrdee@gmail.com>
2017-05-08 15:44:30 +0200

    created Person Class with properties

---
+++ Understanding_InotifyPropertyChanged/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Understanding_InotifyPropertyChanged
+{
+    public class Person
+    {
+
+        private string _firstName;
+        private string _lastName;
+        private string _fullName;
+
+        public string FullName
+        {
+            get { return _fullName = _firstName +" " + _lastName; }
+            set { _fullName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+    }
+}
